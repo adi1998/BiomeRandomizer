58d21668728e366a4377c83346fd1e47ead0590d
Aditya Gupta <adi1998.2010@gmail.com>
2026-01-24 18:03:19 +0530

    UpdateRoomStartMusicEvents to only run events for relevant biomes

--- src/ready.lua
+++ src/ready.lua
@@ -1,12 +1,12 @@
 function mod.dump(o, depth)
    depth = depth or 0
    if type(o) == 'table' then
-      local s = string.rep("\t", depth) .. '{ '
+      local s = "\n" .. string.rep("\t", depth) .. '{\n'
       for k,v in pairs(o) do
          if type(k) ~= 'number' then k = '"'..k..'"' end
          s = s .. string.rep("\t",(depth+1)) .. '['..k..'] = ' .. mod.dump(v, depth + 1) .. ',\n'
       end
-      return s .. string.rep("\t", depth) .. '} \n'
+      return s .. string.rep("\t", depth) .. '}'
    else
       return tostring(o)
    end
@@ -408,8 +408,6 @@ for index, bountyName in ipairs(mod.RegisteredBounties) do
     mod.UpdateRandomBountyOrder(bountyName)
 end
 
-
-
 -- spawn hermes rewards before chronos fight
 -- no styx as it would require a minimum of two paths anyway
 local preFinalBossSet = {
@@ -431,3 +429,47 @@ for _, value in ipairs(preFinalBossSet) do
         FunctionName = _PLUGIN.guid .. "." .. "SpawnShopItemsEarly"
     })
 end
+
+function mod.CheckPathEquality(path1, path2)
+    local flatPath1 = ""
+    local flatPath2 = ""
+    for key, value in pairs(path1) do
+        flatPath1 = flatPath1 .. "." .. tostring(value)
+    end
+    for key, value in pairs(path2) do
+        flatPath2 = flatPath2 .. "." .. tostring(value)
+    end
+    return flatPath1 == flatPath2
+end
+
+function mod.UpdateRoomStartMusicEvents()
+    for musicEventIndex, musicEvent in ipairs(game.RoomStartMusicEvents) do
+        for requireIndex, requirement in ipairs(musicEvent.GameStateRequirements) do
+            if requirement.PathTrue and mod.CheckPathEquality(requirement.PathTrue, { "CurrentRun", "BiomesReached", "F" }) then
+                musicEvent.GameStateRequirements[requireIndex] =
+                {
+					Path = { "CurrentRun", "BiomesReached" },
+					HasAny = { "F", "H", "I", },
+				}
+                table.insert(musicEvent.GameStateRequirements, {
+                    Path = { "CurrentRun", "CurrentRoom", "RoomSetName" },
+                    IsAny = {"F", "H", "I", }
+                })
+            end
+            if requirement.PathTrue and mod.CheckPathEquality(requirement.PathTrue, { "CurrentRun", "BiomesReached", "N" }) then
+                musicEvent.GameStateRequirements[requireIndex] =
+                {
+					Path = { "CurrentRun", "BiomesReached" },
+					HasAny = { "N", "O", "P", "Q", },
+				}
+                table.insert(musicEvent.GameStateRequirements, {
+                    Path = { "CurrentRun", "CurrentRoom", "RoomSetName" },
+                    IsAny = {"N", "O", "P", "Q", "N_SubRooms", }
+                })
+            end
+        end
+    end
+    print(mod.dump(game.RoomStartMusicEvents))
+end
+
+mod.UpdateRoomStartMusicEvents()
